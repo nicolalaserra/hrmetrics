0821a6a4dfd14e2bd22d5e6ba49c86c6c08fae60
Nicola Laserra <nicola.laserra@drivek.com>
2019-05-11 01:25:45 +0200

    chore(): first draft

---
+++ src/components/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hello',
+    data () {
+      return {
+        msg: 'This supersimple App just reads from a predermined csv file and shows some useful graphs'
+      }
+    }
+  }
+</script>
---
+++ src/components/ReportOne.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="reportone">
+        <h1>This will be your first report ever</h1>
+        <p>
+            Let's get our hands dirty with our new report!
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ReportOne'
+    }
+</script>
